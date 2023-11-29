5bc538913c2d4a564b786ae8a54181a08d76efc4
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-11-29 18:43:52 +0100

    creazione progetto partenza spring

---
+++ src/main/java/Manuele/Autonologgeio/AutonologgeioApplication.java
@@ -0,0 +1,13 @@
+package Manuele.Autonologgeio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonologgeioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonologgeioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Manuele/Autonologgeio/AutonologgeioApplicationTests.java
@@ -0,0 +1,13 @@
+package Manuele.Autonologgeio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonologgeioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
